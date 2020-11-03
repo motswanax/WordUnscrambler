7ccc8fab358c081e385ca8b8dc25bdca2a9ea062
bdijo <bdijo@D-IMEC-TRAINEE->
2020-11-03 08:46:42 +0200

    Implemented the FileReader class

--- WordUnscrambler/Workers/FileReader.cs
+++ WordUnscrambler/Workers/FileReader.cs
@@ -1,14 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.IO;
 
 namespace WordUnscrambler.Workers
 {
     class FileReader
     {
-        internal string[] Read(string filename)
+        public string[] Read(string filename)
         {
-            throw new NotImplementedException();
+            string[] fileContent;
+            try
+            {
+                fileContent = File.ReadAllLines(filename);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+
+            return fileContent;
         }
     }
 }
